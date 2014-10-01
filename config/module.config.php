157f682233a0b65c1f17dfddff06235f48471e7d
commiejim <amajot@gmail.com>
2014-10-01 13:40:22 -0400

    added more file

---
+++ config/module.config.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'ugh_authorization' => array(
+    ),
+    'service_manager' => array(
+        'factories' => array(
+        )
+    ),
+    'view_manager' => array(
+        'template_map' => array(
+        )
+    )
+);
