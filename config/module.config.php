4aff6724599621741c94f42c59faea977e48d885
commiejim <amajot@gmail.com>
2014-10-01 14:01:21 -0400

    setting up routes

--- config/module.config.php
+++ config/module.config.php
@@ -3,6 +3,11 @@
 return array(
     'ugh_authorization' => array(
     ),
+    'controllers' => array(
+        'invokables' => array(
+            'UghAuthentication' => '\UghAuthentication\Controller\AuthenticationController',
+        ),
+    ),
     'service_manager' => array(
         'factories' => array(
         )
@@ -10,5 +15,43 @@ return array(
     'view_manager' => array(
         'template_map' => array(
         )
-    )
+    ),
+    'router' => array(
+        'routes' => array(
+            'ughauthentication' => array(
+                'type' => 'Literal',
+                'priority' => 1000,
+                'options' => array(
+                    'route' => '/user',
+                    'defaults' => array(
+                        'controller' => 'ughauthentication',
+                        'action' => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'login' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/login',
+                            'defaults' => array(
+                                'controller' => 'ughauthentication',
+                                'action' => 'login',
+                            ),
+                        ),
+                    ),
+                    'logout' => array(
+                        'type' => 'Literal',
+                        'options' => array(
+                            'route' => '/logout',
+                            'defaults' => array(
+                                'controller' => 'ughauthentication',
+                                'action' => 'logout',
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
 );
--- src/UghAuthentication/Controller/AuthenticationController.php
+++ src/UghAuthentication/Controller/AuthenticationController.php
@@ -3,8 +3,23 @@
 namespace UghAuthentication\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
 
 class UserController extends AbstractActionController
 {
 
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function loginAction()
+    {
+        return new ViewModel();
+    }
+
+    public function logoutAction()
+    {
+        return new ViewModel();
+    }
 }
