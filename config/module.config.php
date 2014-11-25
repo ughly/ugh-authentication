70c014e2bfb26fcc810d333ab07b367cec6de9fc
Yawman, Chris <christopher.yawman@neustar.biz>
2014-11-25 15:32:11 -0500

    Fix stupid

--- config/module.config.php
+++ config/module.config.php
@@ -34,7 +34,7 @@ return array(
     'service_manager' => array(
         'factories' => array(
             'UghAuthentication\Authentication\AuthenticationService' => 'UghAuthentication\Factory\Authentication\AuthenticationService',
-            'UghAuthentication\Form\LoginForm' => 'UghAuthentication\Form\LoginFactory'
+            'UghAuthentication\Form\Login' => 'UghAuthentication\Form\LoginFactory'
         )
     ),
     'view_manager' => array(
